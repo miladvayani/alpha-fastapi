48d4b9408e45751b2f7fa04b569bb4b23662973c
milad_vayani <milad_vayani@yahoo.com>
2022-04-28 10:52:30 +0430

    make docs

--- core/jwt/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    `JWT Plugin is a jwt manager to manage jwt for fastapi.`
-"""
-from .base import JWTManager
-from .helper import token_required
-
-
-__all__ = ["JWTManager", "token_required"]
--- core/middlewares/__init__.py
+++
@@ -1,5 +0,0 @@
-from .middlewares import AuthorizationMiddleware
-from .middlewares import InternationalizationMiddleware
-from .middlewares import BaseHTTPMiddleware
-from .middlewares import ProxyMiddleware
-from .middlewares import ZipkinMiddleware
